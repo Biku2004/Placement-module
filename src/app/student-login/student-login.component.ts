835c796c652fbf3d7138686aa6ba61ad30f15102
Biku2004 <bikash.behera24.04.2004@gmail.com>
2024-10-07 10:48:06 +0530

    placement , page , data page , student dashboard

---
+++ src/app/student-login/student-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-login.component.html',
+  styleUrl: './student-login.component.css'
+})
+export class StudentLoginComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: '#0b235e',
+      },
+    },
   },
   plugins: [],
 }
