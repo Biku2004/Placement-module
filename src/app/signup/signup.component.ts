bf2cb28e59a55c480740eb52bc0736706047e0b8
Biku2004 <bikash.behera24.04.2004@gmail.com>
2024-10-07 11:17:05 +0530

    fixed Bug and connected student button to its signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Import the Router
+// import { StudentLoginComponent } from "../student-login/student-login.component";
 
 @Component({
   selector: 'app-signup',
@@ -8,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  constructor(private router: Router) {} // Inject the Router service
 
+  navigateTo(route: string) {
+    this.router.navigate([route]); // Use this.router to navigate
+  }
 }
