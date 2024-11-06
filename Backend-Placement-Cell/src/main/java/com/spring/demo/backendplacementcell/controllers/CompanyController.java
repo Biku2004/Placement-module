e430d50b91791430613c4c9a910fc972fd320f82
kalinga kishore parida <kalingakishore2003@gmail.com>
2024-11-07 01:22:10 +0530

    Created CompanyController and Repository fir various functions and load on creation in the table

---
+++ Backend-Placement-Cell/src/main/java/com/spring/demo/backendplacementcell/controllers/CompanyController.java
@@ -0,0 +1,37 @@
+// src/main/java/com/spring/demo/backendplacementcell/controllers/CompanyController.java
+package com.spring.demo.backendplacementcell.controllers;
+
+import com.spring.demo.backendplacementcell.entities.Company;
+import com.spring.demo.backendplacementcell.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/companies")
+
+public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+    @PostMapping
+    public Company createCompany(@RequestBody Company company) {
+        return companyService.createCompany(company);
+    }
+
+    @PutMapping("/{id}")
+    public Company updateCompany(@PathVariable Long id, @RequestBody Company company) {
+        return companyService.updateCompany(id, company);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCompany(@PathVariable Long id) {
+        companyService.deleteCompany(id);
+    }
+}
