a991fa414721fe0f6af5ba9898e6e006354c84a1
Ak-Rajak <atulkumar.coderguys@gmail.com>
2024-11-12 21:27:58 +0530

    Modified the about page

--- Frontend/src/app/about/about.component.ts
+++ Frontend/src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
