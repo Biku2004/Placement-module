9ea2a462ba4053c4edcabe9f8f7da5ef0ef7f9f2
kalinga kishore parida <kalingakishore2003@gmail.com>
2024-11-15 01:32:57 +0530

    revert

--- Frontend/src/app/about/about.component.ts
+++ Frontend/src/app/about/about.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [NavbarComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
