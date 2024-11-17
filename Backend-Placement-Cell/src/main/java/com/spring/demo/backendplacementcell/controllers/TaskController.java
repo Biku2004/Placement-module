90e2bc5126a7c14efba9656b8255818a40d26d63
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2024-11-18 01:18:50 +0530

    "Implement Controller For Task"

---
+++ Backend-Placement-Cell/src/main/java/com/spring/demo/backendplacementcell/controllers/TaskController.java
@@ -0,0 +1,37 @@
+package com.spring.demo.backendplacementcell.controllers;
+
+import com.spring.demo.backendplacementcell.entities.Task;
+import com.spring.demo.backendplacementcell.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:4200")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
