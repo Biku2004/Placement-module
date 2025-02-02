354a1bfbcfda98f5707acac83fa8099d9f3e9bde
Biku2004 <bikash.behera24.04.2004@gmail.com>
2025-02-02 21:57:13 +0530

    Made "Create company table" resizable

--- Frontend/src/app/Staff/staff-all/create-company/create-company.component.ts
+++ Frontend/src/app/Staff/staff-all/create-company/create-company.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CompanyService } from './company.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -30,6 +30,9 @@ export class CreateCompanyComponent implements OnInit {
     secondaryContactPhone: ''
   };
   companies: any[] = [];
+  @ViewChild('resizableContainer') resizableContainer!: ElementRef;
+  private defaultWidth = 800;
+  private defaultHeight = 400;
 
   constructor(private companyService: CompanyService) { }
 
@@ -73,4 +76,73 @@ export class CreateCompanyComponent implements OnInit {
       secondaryContactPhone: ''
     };
   }
+
+  deleteSelected(): void {
+    const selectedCompanies = this.companies.filter(company => company.selected);
+    selectedCompanies.forEach(company => {
+      this.companyService.deleteCompany(company.id).subscribe(() => {
+        this.loadCompanies(); // Reload companies after deletion
+      }, error => {
+        console.error('Error deleting company:', error);
+      });
+    });
+  }
+
+  reloadData(): void {
+    this.loadCompanies();
+  }
+
+  resetSize(): void {
+    const div = this.resizableContainer.nativeElement;
+    div.style.width = `${this.defaultWidth}px`;
+    div.style.height = `${this.defaultHeight}px`;
+  }
+
+  ngAfterViewInit(): void {
+    this.makeResizableDiv(this.resizableContainer.nativeElement);
+  }
+
+  toggleSelectAll(event: any): void {
+    const checked = event.target.checked;
+    this.companies.forEach(company => company.selected = checked);
+  }
+
+  makeResizableDiv(div: HTMLElement): void {
+    const resizers = div.querySelectorAll('.resizer');
+    let originalWidth = 0;
+    let originalHeight = 0;
+    let originalMouseX = 0;
+    let originalMouseY = 0;
+
+    resizers.forEach(resizer => {
+      resizer.addEventListener('mousedown', (e) => {
+        e.preventDefault();
+        originalWidth = div.offsetWidth;
+        originalHeight = div.offsetHeight;
+        originalMouseX = (e as MouseEvent).clientX;
+        originalMouseY = (e as MouseEvent).clientY;
+        window.addEventListener('mousemove', resize);
+        window.addEventListener('mouseup', stopResize);
+
+        function resize(e: MouseEvent) {
+          if (resizer.classList.contains('bottom-right')) {
+            div.style.width = originalWidth + (e.clientX - originalMouseX) + 'px';
+            div.style.height = originalHeight + (e.clientY - originalMouseY) + 'px';
+          }
+           else if (resizer.classList.contains('right')) {
+            div.style.width = originalWidth + (e.clientX - originalMouseX) + 'px';
+          } else if (resizer.classList.contains('bottom')) {
+            div.style.height = originalHeight + (e.clientY - originalMouseY) + 'px';
+          }
+        }
+
+        function stopResize() {
+          window.removeEventListener('mousemove', resize);
+          window.removeEventListener('mouseup', stopResize);
+        }
+      });
+    });
+  }
+
+
 }
--- Frontend/src/app/Staff/staff-all/tasks/task-creator/task-creator.component.ts
+++ Frontend/src/app/Staff/staff-all/tasks/task-creator/task-creator.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TaskService } from './task.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
