38a2a484b348015d14f9991d44332254ed9d6109
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-27 14:50:27 +0530

    Created Job Applicants Component For Staff To Manage Jobs

---
+++ Frontend/src/app/Staff/staff-all/job-applicants/job-applicants.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-applicants',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-applicants.component.html',
+  styleUrl: './job-applicants.component.css'
+})
+export class JobApplicantsComponent {
+
+}
