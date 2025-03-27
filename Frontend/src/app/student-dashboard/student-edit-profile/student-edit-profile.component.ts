9212edd18d558ce66082f61b622d4ee21f472161
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-03-27 14:43:21 +0530

    Created Student Edit Profile Component

---
+++ Frontend/src/app/student-dashboard/student-edit-profile/student-edit-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-edit-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-edit-profile.component.html',
+  styleUrl: './student-edit-profile.component.css'
+})
+export class StudentEditProfileComponent {
+
+}
