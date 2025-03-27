9d934db0f231f481708779620935b9bcc77577f5
Sayan Ash <sayanash347200@gmail.com>
2025-03-27 15:12:12 +0530

    Fixed the job service for the staff

--- Frontend/src/app/Staff/staff-all/create-jobs/job.service.ts
+++ Frontend/src/app/Staff/staff-all/create-jobs/job.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -14,7 +15,7 @@ import { Company } from './models/company';
 //   getJobPosts(): Observable<any[]> {
 //     const token = localStorage.getItem('jwt'); // Ensure the token key matches the one used in login
 //     const headers = new HttpHeaders({
-//       'Authorization': Bearer ${token}
+//       'Authorization': `Bearer ${token}`
 //     });
 //     return this.http.get<any[]>(this.apiUrl, { headers });
 //   }
@@ -22,7 +23,7 @@ import { Company } from './models/company';
 //   createJobPost(jobPost: any): Observable<any> {
 //     const token = localStorage.getItem('jwt'); // Ensure the token key matches the one used in login
 //     const headers = new HttpHeaders({
-//       'Authorization': Bearer ${token}
+//       'Authorization': `Bearer ${token}`
 //     });
 //     return this.http.post<any>(this.apiUrl, jobPost, { headers });
 //   }
@@ -30,25 +31,25 @@ import { Company } from './models/company';
 //   updateJobPost(jobPost: any): Observable<any> {
 //     const token = localStorage.getItem('jwt'); // Ensure the token key matches the one used in login
 //     const headers = new HttpHeaders({
-//       'Authorization': Bearer ${token}
+//       'Authorization': `Bearer ${token}`
 //     });
-//     return this.http.put<any>(${this.apiUrl}/${jobPost.id}, jobPost, { headers });
+//     return this.http.put<any>(`${this.apiUrl}/${jobPost.id}`, jobPost, { headers });
 //   }
 
 //   deleteJobPost(id: number): Observable<any> {
 //     const token = localStorage.getItem('jwt'); // Ensure the token key matches the one used in login
 //     const headers = new HttpHeaders({
-//       'Authorization': Bearer ${token}
+//       'Authorization': `Bearer ${token}`
 //     });
-//     return this.http.delete<any>(${this.apiUrl}/${id}, { headers });
+//     return this.http.delete<any>(`${this.apiUrl}/${id}`, { headers });
 //   }
 
 //   sendJobPostToStudents(id: number): Observable<any> {
 //     const token = localStorage.getItem('jwt'); // Ensure the token key matches the one used in login
 //     const headers = new HttpHeaders({
-//       'Authorization': Bearer ${token}
+//       'Authorization': `Bearer ${token}`
 //     });
-//     return this.http.post<any>(${this.apiUrl}/${id}/send, {}, { headers });
+//     return this.http.post<any>(`${this.apiUrl}/${id}/send`, {}, { headers });
 //   }
 
 
@@ -63,12 +64,12 @@ export class JobService {
   private getHeaders(): HttpHeaders {
     const token = localStorage.getItem('jwt');
     return new HttpHeaders({
-        'Authorization': Bearer ${token}
+        'Authorization': `Bearer ${token}`
     });
   }
 
     getJobPosts(): Observable<any[]> {
-        return this.http.get<any[]>(${this.apiUrl}/staff/created/jobs, { headers: this.getHeaders() });
+        return this.http.get<any[]>(`${this.apiUrl}/staff/created/jobs`, { headers: this.getHeaders() });
     }
 
     createJobPost(formData: FormData): Observable<any> {
@@ -76,38 +77,38 @@ export class JobService {
     }
 
     updateJobPost(jobPost: any): Observable<any> {
-        return this.http.put<any>(${this.apiUrl}/${jobPost.id}, jobPost, { headers: this.getHeaders() });
+        return this.http.put<any>(`${this.apiUrl}/${jobPost.id}`, jobPost, { headers: this.getHeaders() });
     }
 
     deleteJobPost(id: number): Observable<any> {
-        return this.http.delete<any>(${this.apiUrl}/${id}, { headers: this.getHeaders() });
+        return this.http.delete<any>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
     }
 
     sendJobPostToStudents(id: number): Observable<any> {
-        return this.http.post<any>(${this.apiUrl}/${id}/send-to-students, {}, { headers: this.getHeaders() });
+        return this.http.post<any>(`${this.apiUrl}/${id}/send-to-students`, {}, { headers: this.getHeaders() });
     }
 
     getJobPostApplications(id: number): Observable<any[]> {
-        return this.http.get<any[]>(${this.apiUrl}/${id}/applications, { headers: this.getHeaders() });
+        return this.http.get<any[]>(`${this.apiUrl}/${id}/applications`, { headers: this.getHeaders() });
     }
 
     shortlistApplication(applicationId: number): Observable<any> {
-        return this.http.put<any>(${this.apiUrl}/applications/${applicationId}/shortlist, {}, { headers: this.getHeaders() });
+        return this.http.put<any>(`${this.apiUrl}/applications/${applicationId}/shortlist`, {}, { headers: this.getHeaders() });
     }
 
     rejectApplication(applicationId: number): Observable<any> {
-        return this.http.put<any>(${this.apiUrl}/applications/${applicationId}/reject, {}, { headers: this.getHeaders() });
+        return this.http.put<any>(`${this.apiUrl}/applications/${applicationId}/reject`, {}, { headers: this.getHeaders() });
     }
 
     updateRoundStatus(applicationId: number, roundName: string, status: string): Observable<any> {
-        return this.http.put<any>(${this.apiUrl}/applications/${applicationId}/rounds/${roundName}, null, {
+        return this.http.put<any>(`${this.apiUrl}/applications/${applicationId}/rounds/${roundName}`, null, {
             headers: this.getHeaders(),
             params: { status }
         });
     }
 
     exportJobApplications(jobId: number): Observable<Blob> {
-        return this.http.get(${this.apiUrl}/${jobId}/applications/export, {
+        return this.http.get(`${this.apiUrl}/${jobId}/applications/export`, {
             headers: this.getHeaders(),
             responseType: 'blob'
         });
@@ -115,7 +116,7 @@ export class JobService {
 
     setExamDetails(applicationId: number, examLink: string, testScheduledTime: string): Observable<any> {
         const params = { examLink, testScheduledTime };
-        return this.http.put<any>(${this.apiUrl}/applications/${applicationId}/exam, null, {
+        return this.http.put<any>(`${this.apiUrl}/applications/${applicationId}/exam`, null, {
             headers: this.getHeaders(),
             params
         });
